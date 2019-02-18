e9d65732b5681bedf594e6486d9f6f5ab62a7d9d
RenoGunray <pike.msonda@gmail.com>
2019-02-18 19:49:18 +0300

    base implentation

--- bbnn/bbnn.py
+++ bbnn/bbnn.py
@@ -1,18 +1,32 @@
 from keras.layers import Input, Dense
-
-
-def input_neuron(input=2, output=2):
-    x =  Input(shape=(input,)) #create tensor 
-    x = Dense(output)(x)
-
-    return x
-    
-def hidden_neuron(x, output=2):
-    x = Dense(output)(x)
-
+import tensorflow as tf
+def create_tensors(input):
+    x = Input(shape=(input,))
     return x
 
-def output_neuron(x, output=2):
+def isTensor(input):
+    if isinstance(input, tf.Tensor):
+        return True
+    return False
+
+def isDense(input):
+    if isinstance(input, type(Dense)):
+        return True
+    return False
+
+def input_neuron(inputs, outputs=[1,1]):
+    input_tensors = []
+    for input in inputs:
+        if(isTensor(input)):
+            input_tensors.append(input)
+        else:
+            input_tensors.append(create_tensors(input))
+
+    x = Dense(outputs[0])(input_tensors[0])
+    x1 = Dense(outputs[1])(input_tensors[1])
+    return x, x1
+
+def output_neuron(x, output=1):
     x = Dense(output)(x)
 
     return x
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from bbnn.bbnn import input_neuron, output_neuron, hidden_neuron
+from bbnn.bbnn import output_neuron, create_tensors, input_neuron
 import numpy as np
 
 if __name__ == "__main__":
-    x = input_neuron()
+    out, out1 = input_neuron(inputs=[1,1])
+    # import pdb; pdb.set_trace()
+    out2, out3 = input_neuron(inputs=[1,out1])
+
+    print(out2)
+    print(out3)
+
     
