c413cc67bff861c61de1974481d063fc0a5bdff1
RenoGunray <pike.msonda@gmail.com>
2019-02-19 09:19:51 +0300

    Added a new implementation

--- bbnn/bbnn.py
+++ bbnn/bbnn.py
@@ -1,4 +1,6 @@
 from keras.layers import Input, Dense
+from keras.layers import Add
+from keras.models import Model
 import tensorflow as tf
 
 """
@@ -26,7 +28,7 @@ def isDense(input):
     Input Neuron. If input is not tensor. Create a tensor. 
     output tensor
 """
-def input_neuron(inputs, outputs=[1,1]):
+def input_neuron(inputs, output=1):
     input_tensors = []
     for input in inputs:
         if(isTensor(input)):
@@ -34,10 +36,26 @@ def input_neuron(inputs, outputs=[1,1]):
         else:
             input_tensors.append(create_tensors(input))
 
-    x = Dense(outputs[0])(input_tensors[0])
-    x1 = Dense(outputs[1])(input_tensors[1])
-    return x, x1
+    x = Dense(output, activation='relu')(input_tensors[0])
+    x1 = Dense(output, activation='relu')(input_tensors[1])
+    return x, x1, input_tensors[0], input_tensors[1]
 
+
+def hidden_neuron(inputs, outputs=[1,1]):
+    hidden_tensors=[]
+    for input, output in zip(inputs,outputs):
+        hidden_tensors.append(Dense(output)(input))
+    
+    return hidden_tensors
+
+
+def add_dense_layer(dense):
+    tensors_to_add = []
+    for d in dense:
+        for t in d:
+            tensors_to_add.append(t)
+        
+    return Add()(tensors_to_add)
 
 """
     output layer function. 
@@ -46,3 +64,8 @@ def output_neuron(x, output=1):
     x = Dense(output, activation="softmax")(x)
 
     return x
+
+def make_model(input, output):
+    model = Model(inputs=input, outputs=output)
+
+    return model
--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from bbnn.bbnn import output_neuron, input_neuron
+from bbnn.bbnn import output_neuron, input_neuron, hidden_neuron, add_dense_layer, make_model
 import numpy as np
+import os
+os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
+from keras.utils import plot_model
 
 if __name__ == "__main__":
-    out, out1 = input_neuron(inputs=[1,1])
-    out2, out3 = input_neuron(inputs=[out,out1])
+    out, out1, in0, in1 = input_neuron(inputs=[1,1]) 
 
-    print(out2)
-    print(out3)
+    out2, out3, in2, in3 = input_neuron(inputs=[1,1])
 
-    
+    hid1 =  hidden_neuron(inputs=[out, out1])
+
+    hid2 =  hidden_neuron(inputs=[out2, out3])
+
+    output =  add_dense_layer([hid1, hid2])
+
+    x = output_neuron(output)
+
+    model = make_model(input=[in0,in1, in2, in3], output=x)
+
+    model.summary()
+
+    plot_model(model, to_file='sample.png')
+
+
